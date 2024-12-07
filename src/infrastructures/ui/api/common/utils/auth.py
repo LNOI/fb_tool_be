031e8e48b845ef6ceb2fb2c24e176960ceff1b74
LNOI <thanhloidev@gmail.com>
2024-12-07 15:05:13 +0700

    update

--- src/infrastructures/ui/api/common/utils/auth.py
+++ src/infrastructures/ui/api/common/utils/auth.py
@@ -4,7 +4,7 @@ from loguru import logger
 
 from starlette import status
 
-from src.middleware import sessions_usecase
+from src.middleware import sessions_usecase, accounts_usecase
 
 credentials_exception = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
@@ -14,6 +14,26 @@ credentials_exception = HTTPException(
 
 
 async def validate_session_token(
+    request: Request,
+) -> UUID:
+    """
+    Validate user from session-token cookie is a uuid4
+    """
+    session_token = request.cookies.get("authjs.session-token")
+
+    if session_token is None:
+        raise credentials_exception
+    try:
+        session = await sessions_usecase.get_session(session_token)
+
+        if session:
+            return session.userId
+    except Exception as e:
+        logger.error(f"Error validating user : {session_token} : {e}")
+    raise credentials_exception
+
+
+async def validate_session_admin_token(
     request: Request,
 ) -> UUID:
     """
